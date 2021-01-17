300e0a16fc9536228be49f84f6abda96a91988a1
Elias Hall <elias.hall@gmail.com>
2021-01-17 14:56:28 -0600

    Initial Commit

---
+++ TipDividerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipDividerSwiftUI
+//
+//  Created by Elias Hall on 1/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipDividerSwiftUI/TipDividerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipDividerSwiftUIApp.swift
+//  TipDividerSwiftUI
+//
+//  Created by Elias Hall on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TipDividerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
