d75a97cfd8cf8feb7fd345aa03e690582f5dc46d
Elias Hall <elias.hall@gmail.com>
2021-01-17 18:52:38 -0600

    Created some Views

--- TipDividerSwiftUI/ContentView.swift
+++ TipDividerSwiftUI/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    var percentage: [Double] = [1.0, 0.10, 0.15, 0.20, 0.25]
+    @State var selectedPercentage: Int = 0
+    @State var textName = ""
+     var numPeopleLabel: Int = 1
+    @State var stepValue: Int = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("TipDivider")
+                .fontWeight(.heavy)
+                .padding()
+            Text("$0.00")
+                .fontWeight(.heavy)
+            Text("Tip: 0.00/person")
+                .fontWeight(.bold)
+            Picker(selection: $selectedPercentage, label: Text(""), content: {
+                Text("None").tag(1)
+                Text("10%").tag(2)
+                Text("15%").tag(3)
+                Text("20%").tag(4)
+                Text("25%").tag(5)
+            }).pickerStyle(SegmentedPickerStyle())
+                .padding()
+            TextField("50", text: $textName)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text("Number of People")
+                .bold()
+            Text("\(numPeopleLabel)")
+                .bold()
+            Stepper("", value: $stepValue).body.frame(width: 100, height: 100, alignment: .center)
+            Button(action: {}, label: {
+                Text("Calculate")
+                    .fontWeight(.bold)
+                    .accentColor(Color.black)
+                    .padding()
+            })
+                }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 11 Pro")
     }
 }
